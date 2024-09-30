009d8ad0bd70bc093ceb7922f9a96a97871cc2bb
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-09-30 16:35:32 -0300

    Controller Music

---
+++ src/main/java/com/music/backend/resources/MusicController.java
@@ -0,0 +1,24 @@
+package com.music.backend.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.music.backend.entities.Music;
+import com.music.backend.service.MusicService;
+
+@RestController
+public class MusicController {
+
+    @Autowired
+    private MusicService service;
+
+    @GetMapping("musics")
+        public List<Music> getAlunos(){
+            return service.getMusics(); 
+        }
+
+    
+}
