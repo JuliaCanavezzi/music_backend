3254119bc94cfa6e15160addbc8839d0d8f807f7
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-09-30 16:18:02 -0300

    Entitie Music

---
+++ src/main/java/com/music/backend/entities/Music.java
@@ -0,0 +1,59 @@
+package com.music.backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity 
+public class Music {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String nameArtist;
+    private String realseDate;
+    private String musicalStyle;
+    private String duration;
+
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getNameArtist() {
+        return nameArtist;
+    }
+    public void setNameArtist(String nameArtist) {
+        this.nameArtist = nameArtist;
+    }
+    public String getRealseDate() {
+        return realseDate;
+    }
+    public void setRealseDate(String realseDate) {
+        this.realseDate = realseDate;
+    }
+    public String getMusicalStyle() {
+        return musicalStyle;
+    }
+    public void setMusicalStyle(String musicalStyle) {
+        this.musicalStyle = musicalStyle;
+    }
+    public String getDuration() {
+        return duration;
+    }
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    
+}
